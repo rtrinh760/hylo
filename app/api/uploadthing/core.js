cabefde23f863d54e6034311c3dce3dc44781ae3
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-11-23 19:54:49 -0500

    Implement file upload via uploadthing

---
+++ app/api/uploadthing/core.js
@@ -0,0 +1,28 @@
+import { currentUser } from "@clerk/nextjs";
+import { createUploadthing } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const user = await currentUser();
+
+      // If you throw, the user will not be able to upload
+      if (!user) throw new Error("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+};
+
+export default ourFileRouter;
