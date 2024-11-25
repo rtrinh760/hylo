abc8ad72cb1b73329beb7d6236a5a98452a06c75
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2024-11-24 20:21:35 -0500

    Add protection to route

--- src/pages/api/subtitles.ts
+++ src/pages/api/subtitles.ts
@@ -1,26 +1,42 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getSubtitles } from 'youtube-captions-scraper';
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSubtitles } from "youtube-captions-scraper";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        const { videoID, lang } = req.query;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
 
-        if (!videoID || !lang) {
-            return res.status(400).json({ error: 'Missing videoID or lang parameter' });
-        }
+  if (!session) {
+    return res.status(401).json({
+      error:
+        "You must be signed in to view the protected content on this page.",
+    });
+  }
 
-        try {
-            const subtitlesResponse = await getSubtitles({
-                videoID: videoID as string,
-                lang: lang as string,
-            });
+  if (req.method === "GET") {
+    const { videoID, lang } = req.query;
 
-            return res.status(200).json(subtitlesResponse);
-        } catch (error) {
-            return res.status(500).json(error);
-        }
-    } else {
-        res.setHeader('Allow', ['GET']);
-        return res.status(405).json({ error: `Method ${req.method} not allowed` });
+    if (!videoID || !lang) {
+      return res
+        .status(400)
+        .json({ error: "Missing videoID or lang parameter" });
     }
+
+    try {
+      const subtitlesResponse = await getSubtitles({
+        videoID: videoID as string,
+        lang: lang as string,
+      });
+
+      return res.status(200).json(subtitlesResponse);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).json({ error: `Method ${req.method} not allowed` });
+  }
 }
