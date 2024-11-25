39d8094c0358b2d0a8f1d4276907ef034c4218e4
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2024-11-24 20:25:37 -0500

    Log errors

--- src/pages/api/subtitles.ts
+++ src/pages/api/subtitles.ts
@@ -33,6 +33,7 @@ export default async function handler(
 
       return res.status(200).json(subtitlesResponse);
     } catch (error) {
+      console.error(error);
       return res.status(500).json(error);
     }
   } else {
