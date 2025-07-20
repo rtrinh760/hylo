81a74f9a490e53d5749fe4c8b9980c18df342623
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2025-07-20 15:24:07 -0700

    remove border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <footer className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex flex-col items-center justify-center py-10 md:gap-2 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
